96dde9bbac65f46f53752d618de5878607e8f3a9
Arsen Tymchuk <a.y.tymchuk@gmail.com>
2025-12-17 00:12:09 +0100

    refactor(infra/mongodb): replace resource_group output with cluster and project names
    Update outputs to better reflect the current infrastructure setup by exposing cluster and project names instead of the resource group name

--- infra/mongodb/outputs.tf
+++ infra/mongodb/outputs.tf
@@ -1,3 +1,7 @@
-output "resource_group" {
-  value = azurerm_resource_group.resourceGroup.name
+output "cluster_name" {
+  value = mongodbatlas_advanced_cluster.atlas_cluster.name
+}
+
+output "project_name" {
+  value = mongodbatlas_project.atlas_project.name
 }
